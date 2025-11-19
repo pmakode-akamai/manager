db2332a94c09935240bfbc8fb0db19a7bc9f2054
pmakode-akamai <pmakode@akamai.com>
2025-11-19 22:09:45 +0530

    Add prefixlist drawer

---
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallPrefixListDrawer.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, Drawer } from '@linode/ui';
+import { useNavigate, useParams } from '@tanstack/react-router';
+import * as React from 'react';
+
+export interface FirewallPrefixListDrawerProps {
+  isOpen: boolean;
+  onClose: () => void;
+  selectedPrefixListLabel: string | undefined;
+}
+
+export const FirewallPrefixListDrawer = React.memo(
+  (props: FirewallPrefixListDrawerProps) => {
+    const { onClose, isOpen, selectedPrefixListLabel } = props;
+    const navigate = useNavigate();
+    const params = useParams({ strict: false });
+
+    const hasRuleSetContext = location.pathname.includes('/ruleset');
+
+    // Call API to get PrefixList by label here
+
+    return (
+      <Drawer
+        onClose={onClose}
+        open={isOpen}
+        title={selectedPrefixListLabel ?? ''}
+      >
+        {selectedPrefixListLabel}
+        <Box marginTop={4}>
+          <Button
+            buttonType="primary"
+            onClick={() => {
+              onClose();
+
+              if (hasRuleSetContext) {
+                navigate({
+                  to: '/firewalls/$id/rules/view/$category/ruleset/$ruleId',
+                  params: {
+                    category: params.category as string,
+                    id: params.id as string,
+                    ruleId: String(params.ruleId),
+                  },
+                });
+              }
+            }}
+          >
+            Back {hasRuleSetContext ? 'To Ruleset' : null}
+          </Button>
+        </Box>
+      </Drawer>
+    );
+  }
+);
