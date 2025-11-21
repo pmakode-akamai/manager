45bb3f737f6a9ae2af2900fce3238fdb85668e6b
pmakode-akamai <pmakode@akamai.com>
2025-11-22 04:31:55 +0530

    Few fixes and some refactoring

--- packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallPrefixListDrawer.tsx
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallPrefixListDrawer.tsx
@@ -24,6 +24,7 @@ export interface PrefixListRuleReference {
   plFirewallIPRef: FirewallIPPrefixListReference;
   type: 'rule' | 'ruleset';
 }
+
 export interface FirewallPrefixListDrawerProps {
   category: Category;
   isOpen: boolean;
@@ -41,13 +42,19 @@ export const FirewallPrefixListDrawer = React.memo(
       useIsFirewallRulesetsPrefixlistsEnabled();
     const { classes } = useStyles();
 
-    const { data, error } = useAllFirewallPrefixListsQuery(
+    const { data, error, isFetching } = useAllFirewallPrefixListsQuery(
       isFirewallRulesetsPrefixlistsEnabled,
       {},
       { name: selectedPrefixListLabel }
     );
 
     const prefixListDetails = data?.[0];
+
+    const isIPv4Supported =
+      prefixListDetails?.ipv4 !== null && prefixListDetails?.ipv4 !== undefined;
+
+    const isIPv6Supported =
+      prefixListDetails?.ipv6 !== null && prefixListDetails?.ipv6 !== undefined;
 
     const isIPv4InUse =
       reference?.plFirewallIPRef === '(IPv4)' ||
@@ -76,192 +83,206 @@ export const FirewallPrefixListDrawer = React.memo(
     return (
       <Drawer
         error={error}
+        isFetching={isFetching}
         onClose={() => onClose({ closeAll: true })}
         open={isOpen}
         title={titleText}
       >
         <Box mt={2}>
-          {[
-            {
-              label:
-                reference?.type === 'ruleset' ? 'Prefix List Name' : 'Name',
-              value: prefixListDetails?.name,
-            },
-            {
-              label: 'ID',
-              value: prefixListDetails?.id,
-              copy: true,
-            },
-            {
-              label: 'Description',
-              value: prefixListDetails?.description,
-              column: true,
-            },
-            {
-              label: 'Type',
-              value: prefixListDetails?.name
-                ? getPrefixListType(prefixListDetails.name)
-                : null,
-            },
-            {
-              label: 'Visibility',
-              value: prefixListDetails?.visibility,
-            },
-            {
-              label: 'Version',
-              value: prefixListDetails?.version,
-            },
-            {
-              label: 'Created',
-              value: prefixListDetails?.created && (
-                <DateTimeDisplay value={prefixListDetails.created} />
-              ),
-            },
-            {
-              label: 'Updated',
-              value: prefixListDetails?.updated && (
-                <DateTimeDisplay value={prefixListDetails.updated} />
-              ),
-            },
-          ].map((item, idx) => (
-            <StyledListItem
-              key={`item-${idx}`}
-              paddingMultiplier={2}
-              sx={
-                item.column
-                  ? { flexDirection: 'column', alignItems: 'flex-start' }
-                  : {}
-              }
-            >
-              {item.label && (
-                <StyledLabel component="span">{item.label}: </StyledLabel>
+          {prefixListDetails && (
+            <>
+              {[
+                {
+                  label:
+                    reference?.type === 'ruleset' ? 'Prefix List Name' : 'Name',
+                  value: prefixListDetails.name,
+                },
+                {
+                  label: 'ID',
+                  value: prefixListDetails.id,
+                  copy: true,
+                },
+                {
+                  label: 'Description',
+                  value: prefixListDetails.description,
+                  column: true,
+                },
+                {
+                  label: 'Type',
+                  value: getPrefixListType(prefixListDetails.name),
+                },
+                {
+                  label: 'Visibility',
+                  value: capitalize(prefixListDetails.visibility),
+                },
+                {
+                  label: 'Version',
+                  value: prefixListDetails.version,
+                },
+                {
+                  label: 'Created',
+                  value: <DateTimeDisplay value={prefixListDetails.created} />,
+                },
+                {
+                  label: 'Updated',
+                  value: <DateTimeDisplay value={prefixListDetails.updated} />,
+                },
+              ].map((item, idx) => (
+                <StyledListItem
+                  key={`item-${idx}`}
+                  paddingMultiplier={2}
+                  sx={
+                    item.column
+                      ? { flexDirection: 'column', alignItems: 'flex-start' }
+                      : {}
+                  }
+                >
+                  {item.label && (
+                    <StyledLabel component="span">{item.label}: </StyledLabel>
+                  )}
+
+                  {item.value}
+
+                  {item.copy && (
+                    <CopyTooltip
+                      className={classes.copyIcon}
+                      text={String(item.value)}
+                    />
+                  )}
+                </StyledListItem>
+              ))}
+
+              {prefixListDetails.deleted && (
+                <StyledListItem paddingMultiplier={2}>
+                  <StyledWarningIcon />
+                  <StyledLabel
+                    component="span"
+                    sx={(theme) => ({
+                      color: theme.tokens.alias.Content.Text.Negative,
+                    })}
+                  >
+                    Marked for deletion:
+                  </StyledLabel>
+                  <DateTimeDisplay
+                    sx={(theme) => ({
+                      color: theme.tokens.alias.Content.Text.Negative,
+                    })}
+                    value={prefixListDetails.deleted}
+                  />
+                  <TooltipIcon
+                    status="info"
+                    text="This Prefix List will be automatically deleted when it’s no longer referenced by other firewalls."
+                  />
+                </StyledListItem>
               )}
 
-              {item.value}
+              {isIPv4Supported && (
+                <Paper
+                  sx={(theme) => ({
+                    backgroundColor: theme.tokens.alias.Background.Neutral,
+                    padding: theme.spacingFunction(12),
+                    marginTop: theme.spacingFunction(8),
+                  })}
+                >
+                  <StyledLabel
+                    sx={(theme) => ({
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      marginBottom: theme.spacingFunction(4),
+                    })}
+                  >
+                    IPv4
+                    <Chip
+                      label={isIPv4InUse ? 'in use' : 'not in use'}
+                      sx={(theme) => ({
+                        background: isIPv4InUse
+                          ? theme.tokens.component.Badge.Positive.Subtle
+                              .Background
+                          : theme.tokens.component.Badge.Neutral.Subtle
+                              .Background,
+                        color: isIPv4InUse
+                          ? theme.tokens.component.Badge.Positive.Subtle.Text
+                          : theme.tokens.component.Badge.Neutral.Subtle.Text,
+                        font: theme.font.bold,
+                        fontSize: theme.tokens.font.FontSize.Xxxs,
+                        marginRight: theme.spacingFunction(6),
+                        flexShrink: 0,
+                      })}
+                    />
+                  </StyledLabel>
 
-              {item.copy && (
-                <CopyTooltip
-                  className={classes.copyIcon}
-                  text={String(item.value)}
-                />
+                  <StyledListItem component="span">
+                    {prefixListDetails.ipv4!.length > 0 ? (
+                      prefixListDetails.ipv4!.join(', ')
+                    ) : (
+                      <i>no IP addresses</i>
+                    )}
+                  </StyledListItem>
+                </Paper>
               )}
-            </StyledListItem>
-          ))}
 
-          {prefixListDetails?.deleted && (
-            <StyledListItem paddingMultiplier={2}>
-              <StyledWarningIcon />
-              <StyledLabel
-                component="span"
-                sx={(theme) => ({
-                  color: theme.tokens.alias.Content.Text.Negative,
-                })}
-              >
-                Marked for deletion:
-              </StyledLabel>
-              <DateTimeDisplay
-                sx={(theme) => ({
-                  color: theme.tokens.alias.Content.Text.Negative,
-                })}
-                value={prefixListDetails.deleted}
-              />
-              <TooltipIcon
-                status="info"
-                text="This Prefix List will be automatically deleted when it’s no longer referenced by other firewalls."
-              />
-            </StyledListItem>
-          )}
-
-          {prefixListDetails?.ipv4 !== null && (
-            <Paper
-              sx={(theme) => ({
-                backgroundColor: theme.tokens.alias.Background.Neutral,
-                padding: theme.spacingFunction(12),
-                marginTop: theme.spacingFunction(8),
-              })}
-            >
-              <StyledLabel
-                sx={(theme) => ({
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  marginBottom: theme.spacingFunction(4),
-                })}
-              >
-                IPv4
-                <Chip
-                  label={isIPv4InUse ? 'in use' : 'not in use'}
+              {isIPv6Supported && (
+                <Paper
                   sx={(theme) => ({
-                    background: isIPv4InUse
-                      ? theme.tokens.component.Badge.Positive.Subtle.Background
-                      : theme.tokens.component.Badge.Neutral.Subtle.Background,
-                    color: isIPv4InUse
-                      ? theme.tokens.component.Badge.Positive.Subtle.Text
-                      : theme.tokens.component.Badge.Neutral.Subtle.Text,
-                    font: theme.font.bold,
-                    fontSize: theme.tokens.font.FontSize.Xxxs,
-                    marginRight: theme.spacingFunction(6),
-                    flexShrink: 0,
+                    backgroundColor: theme.tokens.alias.Background.Neutral,
+                    padding: theme.spacingFunction(12),
+                    marginTop: theme.spacingFunction(8),
                   })}
-                />
-              </StyledLabel>
-
-              <StyledListItem component="span">
-                {prefixListDetails?.ipv4?.join(', ')}
-              </StyledListItem>
-            </Paper>
+                >
+                  <StyledLabel
+                    sx={(theme) => ({
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      marginBottom: theme.spacingFunction(4),
+                      ...(!isIPv6InUse
+                        ? {
+                            color:
+                              theme.tokens.alias.Content.Text.Primary.Disabled,
+                          }
+                        : {}),
+                    })}
+                  >
+                    IPv6
+                    <Chip
+                      label={isIPv6InUse ? 'in use' : 'not in use'}
+                      sx={(theme) => ({
+                        background: isIPv6InUse
+                          ? theme.tokens.component.Badge.Positive.Subtle
+                              .Background
+                          : theme.tokens.component.Badge.Neutral.Subtle
+                              .Background,
+                        color: isIPv6InUse
+                          ? theme.tokens.component.Badge.Positive.Subtle.Text
+                          : theme.tokens.component.Badge.Neutral.Subtle.Text,
+                        font: theme.font.bold,
+                        fontSize: theme.tokens.font.FontSize.Xxxs,
+                        marginRight: theme.spacingFunction(6),
+                        flexShrink: 0,
+                      })}
+                    />
+                  </StyledLabel>
+                  <StyledListItem
+                    component="span"
+                    sx={(theme) => ({
+                      ...(!isIPv6InUse
+                        ? {
+                            color:
+                              theme.tokens.alias.Content.Text.Primary.Disabled,
+                          }
+                        : {}),
+                    })}
+                  >
+                    {prefixListDetails.ipv6!.length > 0 ? (
+                      prefixListDetails.ipv6!.join(', ')
+                    ) : (
+                      <i>no IP addresses</i>
+                    )}
+                  </StyledListItem>
+                </Paper>
+              )}
+            </>
           )}
 
-          {prefixListDetails?.ipv6 !== null && (
-            <Paper
-              sx={(theme) => ({
-                backgroundColor: theme.tokens.alias.Background.Neutral,
-                padding: theme.spacingFunction(12),
-                marginTop: theme.spacingFunction(8),
-              })}
-            >
-              <StyledLabel
-                sx={(theme) => ({
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  marginBottom: theme.spacingFunction(4),
-                  ...(!isIPv6InUse
-                    ? {
-                        color: theme.tokens.alias.Content.Text.Primary.Disabled,
-                      }
-                    : {}),
-                })}
-              >
-                IPv6
-                <Chip
-                  label={isIPv6InUse ? 'in use' : 'not in use'}
-                  sx={(theme) => ({
-                    background: isIPv6InUse
-                      ? theme.tokens.component.Badge.Positive.Subtle.Background
-                      : theme.tokens.component.Badge.Neutral.Subtle.Background,
-                    color: isIPv6InUse
-                      ? theme.tokens.component.Badge.Positive.Subtle.Text
-                      : theme.tokens.component.Badge.Neutral.Subtle.Text,
-                    font: theme.font.bold,
-                    fontSize: theme.tokens.font.FontSize.Xxxs,
-                    marginRight: theme.spacingFunction(6),
-                    flexShrink: 0,
-                  })}
-                />
-              </StyledLabel>
-              <StyledListItem
-                component="span"
-                sx={(theme) => ({
-                  ...(!isIPv6InUse
-                    ? {
-                        color: theme.tokens.alias.Content.Text.Primary.Disabled,
-                      }
-                    : {}),
-                })}
-              >
-                {prefixListDetails?.ipv6?.join(', ')}
-              </StyledListItem>
-            </Paper>
-          )}
           <Button
             buttonType="outlined"
             onClick={() => onClose({ closeAll: false })}
